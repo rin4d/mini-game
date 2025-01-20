a8150c8c84c34049a008bd17bc4ac6f46325ded8
RIN4D <rinad.stn@gmail.com>
2025-01-20 12:07:53 +0100

    Ajout des fichiers statiques et des templates

--- games/views.py
+++ games/views.py
@@ -1,6 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from django.contrib import messages
 
 def home(request):
     return render(request, 'home.html')  # Sans "games/" au début
 
-
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('home')  # Redirige après connexion
+        else:
+            messages.error(request, "Nom d'utilisateur ou mot de passe incorrect.")
+    return render(request, 'games/home.html')
